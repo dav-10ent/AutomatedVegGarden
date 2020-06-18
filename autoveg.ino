4791a5dceffb25a7b5c2f318f53989d277bc8536
dav-10ent <67084817+dav-10ent@users.noreply.github.com>
2020-06-18 14:21:58 +1000

    Update autoveg.ino

--- autoveg.ino
+++ autoveg.ino
@@ -1,8 +1,35 @@
-#define CW 3
-#define CCW 4
+#define SoilPower 7
+#define motor 3
+
+int val = 0;
+int SoilPin = 0;
 
 void setup(){ 
-  pinMode(CW, OUTPUT);
-  pinMode(CCW, OUTPUT);
+  
+  pinMode(motor, OUTPUT);
+  pinMode(soilPower, OUTPUT);
   
   }
+
+void loop(){
+  sensorValue = readSoil();
+  
+  if (sensorValue <= 100){
+    digitalWrite(motor, HIGH);
+    delay(5000);
+    digitalWrite(motor, LOW);
+  }
+  else {
+    delay(1000);
+  }
+}
+
+int readSoil()
+{
+
+    digitalWrite(soilPower, HIGH);//turn D7 "On"
+    delay(10);//wait 10 milliseconds 
+    val = analogRead(soilPin);//Read the SIG value form sensor 
+    digitalWrite(soilPower, LOW);//turn D7 "Off"
+    return val;//send current moisture value
+}
